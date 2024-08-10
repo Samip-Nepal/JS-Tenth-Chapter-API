1385e8d5ef4bf6bf2c3420e7f864e7c28b18fbfe
Samip Nepal <samipnepal5@gmail.com>
2024-08-11 05:38:23 +1000

    Same working using promise chaining

--- First.js
+++ First.js
@@ -5,13 +5,21 @@ let facts=document.querySelectorAll(".para");
 
 let url="https://cat-fact.herokuapp.com/facts";
  
-const getfacts = async ()=>{
+/*const getfacts = async ()=>{
     console.log("Getting data.....");
-    let resposnse=await fetch(url);
-    console.log(resp);
+   let resposnse=await fetch(url);
+   console.log(resp);
   let data=  await resposnse.json();
- facts.innerText=data[0].text;
+  facts.innerText=data[0].text;
     
-};
-getfacts();
+};*/
+  function getfacts()
+  { fetch(url)
+    .then((response)=>{
+    return response.json();
+  }).then((data)=>{
+    console.log(data);
+    facts.innerText=data[0].text;
+  });
+}
 catfacts.addEventListener("clicks",getfacts);
